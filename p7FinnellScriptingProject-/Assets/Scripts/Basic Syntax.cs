e8a59b031dc1d442e4836bc1912d232d53019627
ZCooldudeguy <731256@student.dusd.net>
2025-11-05 15:29:40 -0800

    b
    b

--- p7FinnellScriptingProject-/Assets/Scripts/Basic Syntax.cs
+++ p7FinnellScriptingProject-/Assets/Scripts/Basic Syntax.cs
@@ -7,7 +7,15 @@ public class BasicSyntax : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //thiss line is there to tell me the x position of my object
+        /*Hi There!
+         * this is two lines!
+         * */
+        Debug.Log(transform.position.x);
+        if(transform.position.y <5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
     }
 
     // Update is called once per frame
---
+++ p7FinnellScriptingProject-/Assets/Scripts/If Statements.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+    void TemperatureTest()
+    { // If the coffees temperature is greater than the hottest drinking temperature...
+        if (coffeeTemperature > hotLimitTemperature)
+        { // ... do this
+            print("Coffee is too hot");
+        }
+        // if it isnt but the coffee temperature is lesss than the coldest drinking temperature...
+        else if (coffeeTemperature < coldLimitTemperature)
+            //...do this
+            print("Coffee is too cold");
+        // if neither of those then...
+        else
+        {
+            //...do this
+            print("Coffee is just right");
+        }
+    }
+}
