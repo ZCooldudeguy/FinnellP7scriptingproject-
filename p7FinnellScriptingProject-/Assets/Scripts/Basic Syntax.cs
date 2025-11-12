536652691ec4af04396f0b2cd44793f90e3cbe2c
ZCooldudeguy <731256@student.dusd.net>
2025-11-12 15:28:31 -0800

    scripting project
    synteax

--- p7FinnellScriptingProject-/Assets/Scripts/Basic Syntax.cs
+++ p7FinnellScriptingProject-/Assets/Scripts/Basic Syntax.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicSyntax : MonoBehaviour
+public class BasicSyntax: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //thiss line is there to tell me the x position of my object
+        //this line is there to tell me the x position of my object
+       
         /*Hi There!
          * this is two lines!
          * */
         Debug.Log(transform.position.x);
-        if(transform.position.y <5f)
+        
+        if(transform.position.y <= 5f)
         {
-            Debug.Log("I'm about to hit the ground!");
+            Debug.Log ("I'm about to hit the ground!");
         }
     }
 
