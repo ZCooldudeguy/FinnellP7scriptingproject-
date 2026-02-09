f48098cd85204935672044c3ba874f60d55f8c67
ZCooldudeguy <731256@student.dusd.net>
2026-02-09 15:25:04 -0800

    w
    w

--- p7FinnellScriptingProject-/Assets/Scripts/DestroyOther.cs
+++ p7FinnellScriptingProject-/Assets/Scripts/DestroyOther.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LinearInterpolation : MonoBehaviour
-{/*
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,9 @@ public class LinearInterpolation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.X))
+        {
+            Destroy(other);
+        }
     }
-}*/
-
+}
