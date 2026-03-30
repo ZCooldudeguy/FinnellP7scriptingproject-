3964bb1b4c740ebe69b72a099a8c1818e95e813d
ZCooldudeguy <731256@student.dusd.net>
2026-03-30 14:36:15 -0700

    did arrays
    arrays

--- p7FinnellScriptingProject-/Assets/Scripts/UsingIntantiate.cs
+++ p7FinnellScriptingProject-/Assets/Scripts/UsingIntantiate.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 using System.Collections;
 
-public class RocketDestruction : MonoBehaviour
+public class UsingInstantiate : MonoBehaviour
 {
-    void Start()
+    public Rigidbody rocketPrefab;
+    public Transform barrelEnd;
+
+
+    void Update()
     {
-        Destroy(gameObject, 1.5f);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody rocketInstance;
+            rocketInstance = Instantiate(rocketPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+            rocketInstance.AddForce(barrelEnd.forward * 5000);
+        }
     }
 }
